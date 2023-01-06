7758c9b6441287b8167a5f263a6c53ad7c0000f4
DESKTOP-H5AO8DI\vince <vincewang10a7@gmail.com>
2023-01-07 04:13:52 +0900

    Temp implement vcontainer

---
+++ moba_poc/Assets/Game/Scripts/GameLifeTimeScope.cs
@@ -0,0 +1,21 @@
+﻿using Com.JVL.Game.Managers;
+using UnityEngine.SceneManagement;
+using VContainer;
+using VContainer.Unity;
+
+namespace Com.JVL.Game
+{
+	public class GameLifeTimeScope : LifetimeScope
+	{
+		private GameSceneManager _gameSceneManager;
+		private PlayerManager _playerManager;
+		protected override void Configure(IContainerBuilder builder)
+		{
+			_gameSceneManager ??= new GameSceneManager();
+			_playerManager ??= new PlayerManager();
+			builder.RegisterEntryPoint<GameInstance>();
+			builder.RegisterInstance(_gameSceneManager);
+			builder.RegisterInstance(_playerManager);
+		}
+	}
+}

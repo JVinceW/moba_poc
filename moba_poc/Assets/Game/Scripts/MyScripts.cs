d437990c232784847102f876328c5e900e3c0e12
DESKTOP-H5AO8DI\vince <vincewang10a7@gmail.com>
2023-01-07 02:55:55 +0900

    Organize project structure

--- moba_poc/Assets/Game/Scripts/MyScripts.cs
+++ moba_poc/Assets/Game/Scripts/MyScripts.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class MyScripts : MonoBehaviour
 {
 	// Start is called before the first frame update
-	void Start() { }
+	void Start()
+	{
+		
+	}
 
 	// Update is called once per frame
 	void Update() { }

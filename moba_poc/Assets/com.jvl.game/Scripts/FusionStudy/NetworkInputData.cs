818ae223ec03ae47ce15408a5c4d98a96b940706
DESKTOP-H5AO8DI\vince <vincewang10a7@gmail.com>
2023-02-10 01:40:30 +0900

    Implement Fusion documentation learning 102

---
+++ moba_poc/Assets/com.jvl.game/Scripts/FusionStudy/NetworkInputData.cs
@@ -0,0 +1,13 @@
+﻿using Fusion;
+using UnityEngine;
+
+namespace Com.JVL.Game.FusionStudy
+{
+	public struct NetworkInputData : INetworkInput
+	{
+		public Vector3 direction;
+		public const byte MOUSEBUTTON1 = 0x01;
+
+		public byte buttons;
+	}
+}
